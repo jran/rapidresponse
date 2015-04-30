1ca6fff0c9cb5b7478d179fc58ba535910b0d957
Jenny Hu <jenhu@sas.upenn.edu>
2015-04-30 19:50:05 -0400

    secondtier initial - not working yet

---
+++ pageoperator_web/js/secondTierTesting.js
@@ -0,0 +1,7 @@
+function sendSecondTier(query, alertid){
+	
+	Parse.initialize("MEVkVnjwbter5JAP7mZIeg7747UA1QiBb7mOZ4Ch", "kc6tbhjMB2zRYtkicSxjhwQ8CeqKBIHceFkkGdzG");
+	var submitAlert = Parse.Object.extend("Alert");
+	var query = new Parse.Query(submitAlert);
+
+}
