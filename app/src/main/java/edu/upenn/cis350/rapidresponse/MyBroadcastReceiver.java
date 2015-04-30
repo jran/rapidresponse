1a07bcbccb1702c8a6b20180f5b90d32dc98c413
Jing <jran@sas.upenn.edu>
2015-04-29 20:16:46 -0400

    Customize receiver for sound

---
+++ app/src/main/java/edu/upenn/cis350/rapidresponse/MyBroadcastReceiver.java
@@ -0,0 +1,39 @@
+package edu.upenn.cis350.rapidresponse;
+import com.parse.ParsePushBroadcastReceiver;
+
+
+import android.content.Context;
+import android.content.Intent;
+import android.app.Notification;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.net.Uri;
+import android.support.v4.app.NotificationCompat;
+/**
+ * Created by jran on 2015/4/29.
+ */
+public class MyBroadcastReceiver extends ParsePushBroadcastReceiver {
+    private static final String TAG = "PushNotificationReceiver";
+    private static final int MY_NOTIFICATION_ID=1;
+    NotificationManager notificationManager;
+    Notification myNotification;
+    @Override
+    public void onReceive(Context c, Intent i) {
+        PendingIntent pi = PendingIntent.getBroadcast(c, 0, new Intent("com.example.andtip"),0 );
+        myNotification=new NotificationCompat.Builder(c)
+                .setContentTitle("This is a notification that uses a custom sound.")
+                .setContentText("Notification")
+                .setTicker("Notification!")
+                .setWhen(System.currentTimeMillis())
+                .setContentIntent(pi)
+                .setSound(Uri.parse("android.resource://edu.upenn.cis350.rapidresponse"+R.raw.loudalert))
+                .setAutoCancel(true)
+                .setSmallIcon(R.drawable.ic_launcher)
+                .build();
+
+        notificationManager = (NotificationManager)c.getSystemService(Context.NOTIFICATION_SERVICE);
+        notificationManager.notify(MY_NOTIFICATION_ID, myNotification);
+
+    }
+
+}
