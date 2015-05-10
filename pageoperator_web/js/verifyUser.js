7ffa3d3f3d5ffd1f36196beaff18731d37b88aac
Jenny Hu <jenhu@sas.upenn.edu>
2015-05-10 14:29:23 -0400

    verfiyuser

--- pageoperator_web/js/verifyUser.js
+++ pageoperator_web/js/verifyUser.js
@@ -4,8 +4,8 @@ window.onload  = function() {
     Parse.initialize("MEVkVnjwbter5JAP7mZIeg7747UA1QiBb7mOZ4Ch", "kc6tbhjMB2zRYtkicSxjhwQ8CeqKBIHceFkkGdzG");
 
     var currentUser = Parse.User.current();
-  if (currentUser != null) {
-
+  if (currentUser) {
+    //User may stay on page
   } else {
      // show the signup or login page
      window.location = "login.html";
